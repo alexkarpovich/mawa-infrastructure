d35b1eca7ffb29852142abbb8a3fa2ebed339576
Aliaksandr Karpovich <alexsure.k@gmail.com>
2020-03-23 11:57:20 +0300

    domain and names refactoring

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
-git clone git@github.com:alexkarpovich/mowa-api.git
-git clone git@github.com:alexkarpovich/mowa-ui.git
+git clone git@github.com:alexkarpovich/mowy-api.git
+git clone git@github.com:alexkarpovich/mowy-ui.git
